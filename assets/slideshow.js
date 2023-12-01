55084c5aadcacef879f67e7caa5d41862f64c39c
tanujbordikar <tanujbordikar@gmail.com>
2023-12-01 13:24:50 +0530

    some changes

--- assets/slideshow.js
+++ assets/slideshow.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function () {
     var slideImages = document.querySelectorAll('.hero-slide');
     var current = 0;
@@ -7,54 +6,54 @@ document.addEventListener('DOMContentLoaded', function () {
     var sliderContainer = document.getElementById('sliderContainer');
 
     sliderContainer.addEventListener('mouseenter', function () {
-      clearInterval(interval);
+        clearInterval(interval);
     });
 
     function reset() {
-      for (var i = 0; i < slideImages.length; i++) {
-        slideImages[i].classList.remove('slide-active');
-      }
+        for (var i = 0; i < slideImages.length; i++) {
+            slideImages[i].classList.remove('slide-active');
+        }
     }
 
     function startSlide() {
-      reset();
-      slideImages[0].classList.add('slide-active');
-      document.getElementById('slideCurrent').textContent = current + 1;
+        reset();
+        slideImages[0].classList.add('slide-active');
+        document.getElementById('slideCurrent').textContent = current + 1;
     }
 
     function slideLeft() {
-      reset();
-      slideImages[current - 1].classList.add('slide-active');
-      current--;
+        reset();
+        slideImages[current - 1].classList.add('slide-active');
+        current--;
     }
 
     document.getElementById('prev').addEventListener('click', function () {
-      if (current === 0) {
-        current = slideImages.length;
-      }
-      slideLeft();
-      document.getElementById('slideCurrent').textContent = current + 1;
+        if (current === 0) {
+            current = slideImages.length;
+        }
+        slideLeft();
+        document.getElementById('slideCurrent').textContent = current + 1;
     });
 
     function slideRight() {
-      reset();
-      slideImages[current + 1].classList.add('slide-active');
-      current++;
+        reset();
+        slideImages[current + 1].classList.add('slide-active');
+        current++;
     }
 
     function nextSlide() {
-      if (current === slideImages.length - 1) {
-        current = -1;
-      }
-      slideRight();
-      document.getElementById('slideCurrent').textContent = current + 1;
+        if (current === slideImages.length - 1) {
+            current = -1;
+        }
+        slideRight();
+        document.getElementById('slideCurrent').textContent = current + 1;
     }
 
     document.getElementById('next').addEventListener('click', function () {
-      nextSlide();
+        nextSlide();
     });
 
     document.getElementById('slideTotal').textContent = slideImages.length;
 
     startSlide();
-  });
+});
